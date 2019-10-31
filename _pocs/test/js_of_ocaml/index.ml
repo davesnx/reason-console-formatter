4d7df25e533d2f117639e753da9e95dd6e30a213
David Sancho <dsnxmoreno@gmail.com>
2019-10-31 14:25:06 +0100

    feat: Save jsoo under pocs

---
+++ _pocs/test/js_of_ocaml/index.ml
@@ -0,0 +1,25 @@
+let list = [1; 2; 3] ;;
+
+print_endline "asdfasdf" ;;
+
+let tuple = (4, 5) ;;
+
+let array = [| 1; 2; 3 |] ;;
+
+type variant = OCaml | StandardML ;;
+let lang = OCaml ;;
+
+(* # type host_info =
+    { hostname   : string;
+      os_name    : string;
+      cpu_arch   : string;
+      timestamp  : Time.t;
+    };;
+*)
+
+(* val record : host_info =
+  { hostname = "flick.local";
+    os_name = "Darwin";
+    cpu_arch = "i386";
+    timestamp = 2013-11-05 08:49:38.850439-05:00
+  } ;; *)
