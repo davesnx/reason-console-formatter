68a37ddb4497ba93b242c45d8fdc199e44ba7748
David Sancho <dsnxmoreno@gmail.com>
2019-11-14 12:58:51 +0100

    ci(circle): Skip jest-puppeteer

---
+++ e2e/debug.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer')
+const path = require('path')
+
+;(async () => {
+  const extensionPath = path.join(__dirname, '..', 'extension')
+
+  const browser = await puppeteer.launch({
+    dumpio: true,
+    headless: false,
+    args: [
+      `--disable-extensions-except=${extensionPath}`,
+      `--load-extension=${extensionPath}`,
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage'
+    ]
+  })
+
+  console.log('browser starting')
+  const page = await browser.newPage()
+  console.log('browser started')
+  await page.goto('https://example.com/', { waitUntil: 'domcontentloaded' })
+  console.log('page connected')
+  await page.waitFor(5000)
+  const title = await page.title()
+  console.log('title is', title)
+  await browser.close()
+})()
